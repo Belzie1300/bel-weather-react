d5df669b1d1633d1030781d051727fee837ed851
Belzie <dinh.belinda@gmail.com>
2022-12-08 17:40:38 +1100

    Installed axios and made sure the api call worked. useState to update content

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -1,67 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherSummary.css";
+import axios from "axios";
 
 export default function WeatherSummary() {
-  let weatherData = {
-    city: "Osaka",
-    temp: 18,
-    date: "Wednesday 15:00",
-    maxTemp: 21,
-    minTemp: 11,
-    description: "Clear",
-    humidity: 40,
-    wind: 5,
-    iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-  };
-  return (
-    <div className="WeatherSummary">
-      <div className="row ">
-        <div className="col-3 icon-summary">
-          <img src={weatherData.iconUrl} alt={weatherData.description} />
-          <div className="description">{weatherData.description}</div>
-        </div>
-        <div className="col-5">
-          <div className="weather-temperature">
-            <span className="main-temp">
-              <strong>{weatherData.temp}</strong>
-            </span>
-            <span className="unit">
-              <a href="/">°C</a> | <a href="/">°F</a>
-            </span>
+  const [loaded, setLoaded] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    setWeatherData({
+      city: response.data.city,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
+    });
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherSummary">
+        <div className="row ">
+          <div className="col-3 icon-summary">
+            <img src={weatherData.icon} alt={weatherData.description} />
+            <div className="description">{weatherData.description}</div>
           </div>
-        </div>
-        <div className="col-4">
-          <div>
+          <div className="col-5">
+            <div className="weather-temperature">
+              <span className="main-temp">
+                <strong>{Math.round(weatherData.temperature)}</strong>
+              </span>
+              <span className="unit">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+          <div className="col-4">
             <div>
               <ul className="main-overview">
                 <li>
-                  Max:{" "}
-                  <span className="main-overview-max">
-                    {weatherData.maxTemp}
-                  </span>
-                  °C
+                  Humidity:{" "}
+                  <span className="humidity">{weatherData.humidity}</span>%
                 </li>
                 <li>
-                  Min:{" "}
-                  <span className="main-overview-min">
-                    {weatherData.minTemp}
-                  </span>
-                  °C
+                  Wind:{" "}
+                  <span className="wind">{Math.round(weatherData.wind)}</span>{" "}
+                  km/h
                 </li>
               </ul>
             </div>
-            <ul className="main-overview">
-              <li>
-                Humidity:{" "}
-                <span className="humidity">{weatherData.humidity}</span>%
-              </li>
-              <li>
-                Wind: <span className="wind">{weatherData.wind}</span> km/h
-              </li>
-            </ul>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Osaka";
+    let apiKey = "34b7a5f9e78b04eob5f3acad597t59d2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Please wait...";
+  }
 }
